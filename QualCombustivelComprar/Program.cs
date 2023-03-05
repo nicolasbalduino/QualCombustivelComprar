55b2c7eea51bd33ac62a0d499dfe0f34678410e8
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 20:36:51 -0300

    reducao de codigo facultativo para a IDE

--- QualCombustivelComprar/Program.cs
+++ QualCombustivelComprar/Program.cs
@@ -1,18 +1,12 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        float precoAlcool, precoGasolina;
+﻿float precoAlcool, precoGasolina;
 
-        Console.WriteLine("Informe o preco do alcool: ");
-        precoAlcool = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe o preco do alcool: ");
+precoAlcool = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Informe o preco da gasolina: ");
-        precoGasolina = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe o preco da gasolina: ");
+precoGasolina = float.Parse(Console.ReadLine());
 
-        if (precoAlcool / precoGasolina <= 0.72)
-            Console.WriteLine("Compre alcool");
-        else
-            Console.WriteLine("Compre Gasolina");
-    }
-}
+if (precoAlcool / precoGasolina <= 0.72)
+    Console.WriteLine("Compre alcool");
+else
+    Console.WriteLine("Compre Gasolina");
