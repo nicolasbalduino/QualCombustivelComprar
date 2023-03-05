6a2aec8204f04c664047a79031dcecdabbe0b0b7
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 20:42:54 -0300

    criada funcao que retorna a razao entre os combustiveis

--- QualCombustivelComprar/Program.cs
+++ QualCombustivelComprar/Program.cs
@@ -1,12 +1,17 @@
 ﻿float precoAlcool, precoGasolina;
 
+float RazaoCombustivel()
+{
+    return precoAlcool / precoGasolina;
+}
+
 Console.WriteLine("Informe o preco do alcool: ");
 precoAlcool = float.Parse(Console.ReadLine());
 
 Console.WriteLine("Informe o preco da gasolina: ");
 precoGasolina = float.Parse(Console.ReadLine());
 
-if (precoAlcool / precoGasolina <= 0.72)
+if (RazaoCombustivel() <= 0.72)
     Console.WriteLine("Compre alcool");
 else
     Console.WriteLine("Compre Gasolina");
