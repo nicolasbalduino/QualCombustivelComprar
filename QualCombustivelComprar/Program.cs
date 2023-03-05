6dbaea5dfd94d9524ca37900b0bb92b73cef155f
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 15:31:58 -0300

    Add project files.

---
+++ QualCombustivelComprar/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float precoAlcool, precoGasolina;
+
+        Console.WriteLine("Informe o preco do alcool: ");
+        precoAlcool = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o preco da gasolina: ");
+        precoGasolina = float.Parse(Console.ReadLine());
+
+        if (precoAlcool / precoGasolina <= 0.72)
+            Console.WriteLine("Compre alcool");
+        else
+            Console.WriteLine("Compre Gasolina");
+    }
+}
